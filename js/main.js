7688d90ae85c52207a42bbab10b7faf86e67a01e
mykytiakv <vasyamc@gmail.com>
2018-06-16 22:24:33 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,11 +2,24 @@ $(document).ready(function() {
 	
 	setTimeout(function(){
 		$('body').addClass('loaded');
-		$('h1').css('color','#222222');
 	}, 100);
-	
+
+	changeWidth();
 });
 
+window.addEventListener("resize", changeWidth);
+
+function changeWidth() {
+	if (window.innerWidth <= 425) {
+		document.getElementsByClassName('l-screen')[0].style.display = 'none';
+		document.getElementsByClassName('r-screen')[0].style.display = 'none';
+		document.getElementsByClassName('screen-list-parent')[0].style.width = '100%';
+	} else {
+		document.getElementsByClassName('l-screen')[0].style.display = 'block';
+		document.getElementsByClassName('r-screen')[0].style.display = 'block';
+		document.getElementsByClassName('screen-list-parent')[0].style.width = '85%';
+	}
+}
 new WOW().init();
 window.onscroll = function() { 
 	var header = document.getElementsByTagName('header')[0];
